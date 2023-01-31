357d2cdcc462c9f333ba79cb3954d156fe506046
rkessal <kessal.rayhan25@gmail.com>
2023-01-31 15:54:37 +0100

    init

---
+++ lua/rayhan/set.lua
@@ -0,0 +1,8 @@
+vim.opt.nu = true
+vim.opt.relativenumber = true
+vim.opt.autoindent = true
+vim.opt.scrolloff = 10
+vim.opt.smarttab = true
+vim.opt.breakindent = true
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
