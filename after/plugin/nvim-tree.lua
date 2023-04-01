c1b840f1670de0b38448b8a3d938d8e948fcbc4a
rkessal <kessal.rayhan25@gmail.com>
2023-04-01 17:14:52 +0200

    nvimtree tweaks

--- after/plugin/nvim-tree.lua
+++ after/plugin/nvim-tree.lua
@@ -7,26 +7,51 @@ vim.g.loaded = 1
 vim.g.loaded_netrwPlugin = 1
 
 vim.cmd([[ highlight NvimTreeIndentMarker guifg=#3FC5FF ]])
+
 nvimtree.setup({
+	disable_netrw = true,
+	hijack_cursor = true,
 	diagnostics = {
 		enable = true,
 		show_on_dirs = true,
+		icons = {
+			hint = "H",
+			info = "I",
+			warning = "W",
+			error = "E",
+		},
 	},
 	renderer = {
+		highlight_git = true,
 		icons = {
+			git_placement = "after",
+			show = {
+				folder_arrow = false,
+			},
 			glyphs = {
-				folder = {
-					arrow_closed = "", -- arrow when folder is closed
-					arrow_open = "", -- arrow when folder is open
+				git = {
+					unstaged = "U",
+					staged = "S",
+					unmerged = "",
+					renamed = "R",
+					untracked = "N",
+					deleted = "D",
+					ignored = "◌",
 				},
 			},
 		},
+	},
+
+	view = {
+		width = 35,
+		hide_root_folder = true,
 	},
 	-- disable window_picker for
 	-- explorer to work well with
 	-- window splits
 	actions = {
 		open_file = {
+			quit_on_open = true,
 			window_picker = {
 				enable = false,
 			},
